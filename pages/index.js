92c98bfd5dcd897799c836132bfc19db187e94b5
Shiv <pals55002@gmail.com>
2021-11-01 13:18:27 +0530

    footer done

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,21 @@
 import Head from 'next/head'
-
+import styles from '../styles/home.module.css'
 
 export default function Home() {
   return (
     <div>
       <Head>
-        <title>TextUtils</title>
+        <title>PekaPay</title>
         <meta
       name="description"
       content="TextUtils is a utility which can be used to manipulate your text in the way you want."
     />
        
       </Head>
-     
-      <h1>hoo</h1>
+
+      <button type="button" class="bg-gradient-to-r from-green-400 to-blue-500 hover:from-pink-500 hover:to-yellow-500 md:from-blue-500 md:focus:from-blue-700">
+  Focus me
+</button>
     </div>
   )
 }
